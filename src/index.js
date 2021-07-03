e7e719d705ec07b513b8e04e2a106db1da3460a5
sachin <sachinbhattarai55@gmail.com>
2021-07-03 15:32:33 +0545

    forever aded

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 
-const PORT = 8000;
 
 dotenv.config({path:'./userConfig.env'});
 const app = express();
@@ -16,25 +15,26 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded(
   {extended:true}
-));
+  ));
+  
+  app.use('/',Router(router));
+  
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
 
-app.use('/',Router(router));
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-
-  const statusCode = err.statusCode;
-  const message = err.message;
-
-  return res
+    const statusCode = err.statusCode;
+    const message = err.message;
+    
+    return res
     .status(statusCode)
     .json({
       status: 'error',
       statusCode,
       message,
     });
-});
-
-app.listen(PORT,()=>{
-  console.log(`Server up and running on ▶️  ${PORT} ◀️`);
-});
+  });
+  const PORT = process.env.PORT;
+  
+  app.listen(PORT,()=>{
+    console.log(`Server up and running on ▶️  ${PORT} ◀️`);
+  });
